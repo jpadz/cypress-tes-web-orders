547d9b852a565349f89d4972c3941f5a690efc1e
Jeuel <jeuelpadecio27@gmail.com>
2025-09-03 11:46:35 +0800

    Initial commit

---
+++ cypress/e2e/web-orders.cy.js
@@ -0,0 +1,12 @@
+/// <reference types="cypress" />
+
+
+
+describe('template spec', () => {
+  beforeEach(() => {
+cy.visit('http://secure.smartbearsoftware.com/samples/TestComplete12/WebOrders/Login.aspx?ReturnUrl=%2fsamples%2ftestcomplete12%2fweborders%2fDefault.aspx')
+  })
+  it('passes', () => {
+    
+  })
+})
