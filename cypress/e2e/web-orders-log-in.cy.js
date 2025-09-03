fcc9ba02668c5a07f0dbc1d5376d6ed9a15d2412
Jeuel <jeuelpadecio27@gmail.com>
2025-09-03 12:24:57 +0800

    Update: describe your changes here

--- cypress/e2e/web-orders-log-in.cy.js
+++ cypress/e2e/web-orders-log-in.cy.js
@@ -2,11 +2,13 @@
 
 
 
-describe('template spec', () => {
+describe('web-order-log-in', () => {
   beforeEach(() => {
 cy.visit('http://secure.smartbearsoftware.com/samples/TestComplete12/WebOrders/Login.aspx?ReturnUrl=%2fsamples%2ftestcomplete12%2fweborders%2fDefault.aspx')
   })
-  it('passes', () => {
-    
+  it('Log In', () => {
+    cy.fixture("user").then((user) => {
+      cy.login(user.email, user.password)
+    })    
   })
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,9 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add("login", (email, password) => {
+
+  cy.get('#ctl00_MainContent_username').type(email);
+  cy.get('#ctl00_MainContent_password').type(password); 
+  cy.get('#ctl00_MainContent_login_button').click();
+});
